df33137b63a3182e4dc4c291ac8dd7268a92b859
yaharong <bandinyou@naver.com>
2017-08-03 20:13:23 +0900

    8월 3일 이재혁

---
+++ app/src/main/java/com/example/togi/ggock/ui/FranceActivity.java
@@ -0,0 +1,15 @@
+package com.example.togi.ggock.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.togi.ggock.R;
+
+public class FranceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_france);
+    }
+}
